1308f92d7a7eb5c7d1de16b874415e1d2c7eaf6e
Ben Wernicke <ben@wernicke.at>
2025-03-30 03:28:24 +0200

    some basic todo operations

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -6,12 +6,12 @@ use std::ops::DerefMut;
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub struct ID<T> {
-    id: usize,
+    id: i64,
     marker: PhantomData<T>,
 }
 
 impl<T> ID<T> {
-    pub fn new(id: usize) -> Self {
+    pub fn new(id: i64) -> Self {
         Self {
             id,
             marker: PhantomData,
@@ -23,13 +23,13 @@ impl<T> std::default::Default for ID<T> {
     fn default() -> Self {
         Self {
             id: 0,
-            marker: PhantomData
+            marker: PhantomData,
         }
     }
 }
 
 impl<T> Deref for ID<T> {
-    type Target = usize;
+    type Target = i64;
 
     fn deref(&self) -> &Self::Target {
         &self.id
@@ -44,7 +44,7 @@ impl<T> DerefMut for ID<T> {
 
 impl<T> FromSql for ID<T> {
     fn column_result(value: rusqlite::types::ValueRef<'_>) -> rusqlite::types::FromSqlResult<Self> {
-        let id: usize = <usize as FromSql>::column_result(value)?;
+        let id: i64 = <i64 as FromSql>::column_result(value)?;
         Ok(Self {
             id,
             marker: PhantomData,
@@ -64,12 +64,131 @@ pub enum TodoState {
     Done,
 }
 
+impl ToSql for TodoState {
+    fn to_sql(&self) -> rusqlite::Result<ToSqlOutput<'_>> {
+        let state: i64 = match self {
+            TodoState::Todo => 0,
+            TodoState::Done => 1,
+        };
+        Ok(ToSqlOutput::Owned(rusqlite::types::Value::Integer(state)))
+    }
+}
+
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct Todo {
     pub title: String,
     pub body: String,
-    pub id: ID<Todo>,
-    pub scheduled: NaiveDate,
-    pub deadline: NaiveDate,
+    pub id: Option<ID<Todo>>,
+    pub scheduled: Option<NaiveDate>,
+    pub deadline: Option<NaiveDate>,
     pub state: TodoState,
+}
+
+impl Todo {
+    pub fn new(
+        title: String,
+        body: String,
+        scheduled: Option<NaiveDate>,
+        deadline: Option<NaiveDate>,
+    ) -> Self {
+        Self {
+            title,
+            body,
+            id: None,
+            scheduled,
+            deadline,
+            state: TodoState::Todo,
+        }
+    }
+
+    pub fn fetch_all(db: &rusqlite::Connection) -> Result<Vec<Self>, anyhow::Error> {
+        todo!()
+    }
+
+    pub fn insert(&mut self, db: &rusqlite::Connection) -> Result<(), anyhow::Error> {
+        assert!(self.id.is_none());
+
+        let mut stmt = db.prepare_cached(
+            "
+            INSERT INTO todos
+                (title, body, scheduled, deadline, state)
+            VALUES
+                (?, ?, ?, ?, ?)
+            RETURNING id;
+            ",
+        )?;
+
+        let id = stmt.query_row(
+            rusqlite::params![
+                self.title,
+                self.body,
+                self.scheduled.map(|d| d.to_string()),
+                self.deadline.map(|d| d.to_string()),
+                self.state
+            ],
+            |row| row.get(0),
+        )?;
+
+        self.id = id;
+
+        Ok(())
+    }
+
+    pub fn update(&self, db: &rusqlite::Connection) -> Result<(), anyhow::Error> {
+        let mut stmt = db.prepare_cached(
+            "
+            UPDATE todos SET
+                title = ?,
+                body = ?,
+                scheduled = ?,
+                deadline = ?,
+                state = ?
+            WHERE id = ?;
+            ",
+        )?;
+
+        stmt.execute(rusqlite::params![
+            self.title,
+            self.body,
+            self.scheduled.map(|d| d.to_string()),
+            self.deadline.map(|d| d.to_string()),
+            self.state,
+            self.id
+        ])?;
+        Ok(())
+    }
+
+    pub fn upsert(&mut self, db: &rusqlite::Connection) -> Result<(), anyhow::Error> {
+        match self.id {
+            Some(_) => self.update(db),
+            None => self.insert(db),
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_todo_insert() {
+        let db = rusqlite::Connection::open_in_memory().unwrap();
+        mittelmeer::migrate(&db).unwrap();
+
+        let mut todo = Todo::new("title".into(), "body".into(), None, None);
+        todo.insert(&db).unwrap();
+        assert!(todo.id.is_some());
+    }
+
+    #[test]
+    fn test_todo_update() {
+        let db = rusqlite::Connection::open_in_memory().unwrap();
+        mittelmeer::migrate(&db).unwrap();
+
+        let mut todo = Todo::new("title".into(), "body".into(), None, None);
+        todo.insert(&db).unwrap();
+        assert!(todo.id.is_some());
+        todo.title = "some other title".into();
+        todo.update(&db).unwrap();
+    }
 }
